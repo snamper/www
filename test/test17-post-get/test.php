0e30411a1539fcd6776442edc45525d6929eaeac
程文乐 <245179840@qq.com>
2017-07-07 17:46:15 +0800

    Default
    Signed-off-by: 程文乐 <245179840@qq.com>

--- test/test17-post-get/test.php
+++ test/test17-post-get/test.php
@@ -1,7 +1,8 @@
 <?php
 var_dump($_REQUEST);
 ?>
-<form action='' method="post">
+<form action='' method="get">
     <input type="text" name='text' >
     <input type="submit" value="1111">
 </form>
+564666656565
